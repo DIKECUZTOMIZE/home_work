f41f127b29d7ac4ddc3191b5b304b22e6413406d
sanjit <sanjitkeleng5@gmai.com>
2024-11-19 04:24:21 -0500

    add

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:home_work/1.stateLess/1.Strobery_Ui/app.dart';
 
 import 'package:home_work/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(StroberryApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
